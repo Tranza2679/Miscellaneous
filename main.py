b3e7a73d483ce45d748568d1b94ea6492e5631ac
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-09-30 01:38:26 +0000

    added changes

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#Tic Tac 
+#Tic tac toe
+
+import random 
+user_input = input("Will you play X or O?")
+one = "_"
+two = "_"
+three = "_"
+four = "_"
+five = "_"
+six ="_"
+seven = "_"
+eight = "_"
+nine = "_"
+def board():
+    print(f"  |  |  ")
+    print(f"_")
+    print(f"__|__|__")
+    print(f"  |  | ")
+first_turn = int(input("Where are you going to place X/O?"))
